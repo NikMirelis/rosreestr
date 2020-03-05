c27c0b45a7db767b2f1b7af809f9f37d7758fda7
NikMirelis <retori.kiri@gmail.com>
2020-03-05 17:14:06 +0500

    Header + nav + Пополнить баланс

--- src/app/js/common.js
+++ src/app/js/common.js
@@ -1,5 +1,23 @@
 $(function() {
-
-	// Custom JS
+	$(document).ready(function() {
+		$("#all-services").mmenu({
+			 // options
+		}, {
+			 // configuration
+			 offCanvas: {
+				 
+			 }
+		});
+ });
+ $(document).ready(function() {
+	$("#personal-info").mmenu({
+		 // options
+	}, {
+		 // configuration
+		 offCanvas: {
+			 
+		 }
+	});
+});
 
 });
